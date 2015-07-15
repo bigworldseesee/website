4e2c5d381b5793db872e407feecb1662f83433f8
dracifer <dracifer@gmail.com>
2015-07-14 23:51:13 -0400

    correct populater

--- app/models/dbPopulator/populate_os_metadata.js
+++ app/models/dbPopulator/populate_os_metadata.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
-var configDB = require('../../../config/database');
+var config = require('../../../config/config');
 var osMetadata = require('../os_metadata');
 
-mongoose.connect(configDB.url, function(err){
+mongoose.connect(config.database.url, function(err){
     if(err) throw err;
-    console.log('connecgted ' + configDB.url);
+    console.log('connecgted ' + config.database.url);
 
 var upload = function(){
     var url = '';
